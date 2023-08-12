8dce3997562f35aa84ee8db2663d49a59da9d87c
pasaismihan <pasa_13142@hotmail.com>
2023-08-12 13:44:21 +0300

    ORM Yaklaşımı

---
+++ EF Core/Program.cs
@@ -0,0 +1,5 @@
+﻿// Biz yazilim uygulamarinda verileri fiziksel olarak veri tabaninda tutariz , bu da cogunlukla SQL olur . Bu veri tabani ile yazilim arasinda baglanti saglamamiz gerekir ki gelen verileri kaydedip , istenilen veriyi geri gondermek icin . bu durumda ORM mantigi devreye gier 
+// ORM -> object relational mapping anlamina gelir . Yazilimdan SQL sorgularini soyutlamak ve OOP nesnesi nimetlerinden yararlanilarak kullanilan bir yaklasimdir
+// nesne dedigimizde bir class demistik bu durumda yapilmak istenen SQL database ler icin bir class olusturmak , tablolar icin ayri bir class olusturmak ve bu veri tabani classinin tablo classini referans etmesini saglariz ve iliskilendirme yapariz bu ORM mantigidir
+// ORM deki iliskisel eslestirme mantigina gore ornek su sekildedir NorthwindDbContext context = new();   ->     var employeesDatas = await context.Employees.toListAsync(); bu yapilan islemde
+// context ref adresi bize tum databaseyi isaret eder . sonrasinda yapilan islemde context.Employees dedigimizde employees de bulunan employeesId , firstname , lastname ... her biri satiri 1 nesne olacak sekilde bizim onumuze sunar
