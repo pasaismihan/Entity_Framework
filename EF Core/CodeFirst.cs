5ad8cff63208356ff59533b6a861ed5af7655b1f
pasaismihan <pasa_13142@hotmail.com>
2023-08-13 18:52:28 +0300

    CodeFirst Yaklasimi Uygulama Asamalari

---
+++ EF Core/CodeFirst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Application
+{
+	public class CodeFirst
+	{
+		public CodeFirst()
+		{
+			//Codefirst de domain classlari yani kodla sifirdan olusturacagimiz tablolari ve kolonlari migrate ederek databaseye aktariyoruz
+		}
+	}
+}
+
