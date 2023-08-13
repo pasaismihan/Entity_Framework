0a40b06f8a5b87c5d4529fc90e72a25fa16c9a22
pasaismihan <pasa_13142@hotmail.com>
2023-08-13 18:13:25 +0300

    Partial ile Entity Update

--- EF Core/DatabaseFirst.cs
+++ EF Core/DatabaseFirst.cs
@@ -18,6 +18,9 @@ namespace EF_Core
             // Projede dbcontext ve entity ler default olarak projenin kok dizinine gelir fakat bunlari farkli path(ayri dosyalar) olarak ayarlamak istersek kodumuzun sonuna PMC icin -ContextDir context -OutputDir entities seklinde yazabiliriz boylece ayri dosyalar icerisinde olustururuz
             // yukaridaki islemi dotnet CLI icin olusturmak istersek kodumuzun sonuna --context-dir context --output-dir entities yazariz boylece contextimiz context dosyasi icinde , entitiler de entities dosyasi icerisinde olusur
             // ayni path i manuel olusturdugumuz gibi namespaceleri de kodlama ile olusturabiliriz -Namespace Entities -ContextNamespace Context ile ayri ayri namespacelerini manuel olarak olustururuz
+            // eger ki entitiy ler uzerinde degisiklik yapmak istersek veya ekleme yaparsak databaseden PMC veya dotnet CLI ile tablolari cekersek bize hata verecektir degisiklik oldugu icin fakat kodumuzun sonunda -force eklersek
+            // bizim yaptigimiz degisikligi ezer ve database tablolari ne ise entity olarak onlari verir . bu sekilde ezilmesini istemiyorsak Partial path olustururuz ve degistirmek istedigimiz entity yi buraya kopyalayip degisiklikleri bunun uzerinde yapariz
+            // normalde ayni namespace altinda ayni isimde 2 farkli entity olusturamayiz fakat partial yapacaksak class in onune partial keywordunu ekleriz bu sekilde -force yapsak da yazdigimiz ekstra kodlar ezilmez 
         }
     }
 }
